f47dae24bbba81b3289ff240cca2ed72173985e0
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-05-15 17:35:22 +0200

    answer-task4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,6 +1,7 @@
 const value = document.querySelector("#value");
 const decrement = document.querySelector('button[data-action="decrement"]')
 const increment = document.querySelector('button[data-action="increment"]');
+
 decrement.addEventListener("click", function () {
     value.innerHTML = value.innerHTML - 1;
 })
