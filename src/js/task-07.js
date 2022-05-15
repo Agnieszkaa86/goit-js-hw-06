2837e26835d89a5ac580cccc6e2eed3e9c850bad
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-05-15 17:36:15 +0200

    answer-task7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#font-size-control");
 const span = document.querySelector("#text");
 
+
 input.addEventListener("input", (event) => {
   const  value = event.currentTarget.value;
    span.style.fontSize = `${value}px`;
