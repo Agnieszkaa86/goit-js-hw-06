f02d10ac053c553e5dd8cb5a1fb6dbaf7bb9b0bb
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-05-15 17:35:37 +0200

    answer-task5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,5 +1,6 @@
 const input = document.querySelector("#name-input")
 const output = document.querySelector("#name-output")
+
 input.addEventListener("input", (event) => {
     const text = event.currentTarget.value;
     if (text.length > 0) {
