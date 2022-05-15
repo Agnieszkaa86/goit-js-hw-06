26d3625c96e2cf7cb51010dc7da11663e4e90c58
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-05-15 17:34:26 +0200

    answer-task1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,5 +1,6 @@
 const liItems = document.querySelectorAll(".item");
 console.log(`Number of categories: ${liItems.length}`);
+
 const heading = document.querySelectorAll("h2");
 console.log(`Category: ${heading[0].innerHTML}`);
 console.log(`Elements: ${liItems[0].children[1].children.length}`);
